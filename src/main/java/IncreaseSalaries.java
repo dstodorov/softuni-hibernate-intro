9346527ce2e051504151c5d440223571dc44a33b
Dimitar Todorov <d.todorov.vn@gmail.com>
2022-10-30 21:23:34 +0200

    Add exercise

--- src/main/java/IncreaseSalaries.java
+++ src/main/java/IncreaseSalaries.java
@@ -1,2 +1,31 @@
+import entities.Employee;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+import java.math.BigDecimal;
+
 public class IncreaseSalaries {
+    private static final String ENGINEERING_DEPT = "Engineering";
+    private static final String TOOL_DESIGN_DEPT = "Tool Design";
+    private static final String MARKETING_DEPT = "Marketing";
+    private static final String INFORMATION_SERVICES_DEPT = "Information Services";
+
+    public static void main(String[] args) {
+        EntityManager entityManager = Persistence.createEntityManagerFactory("soft_uni").createEntityManager();
+        entityManager.getTransaction().begin();
+
+        entityManager.createQuery("SELECT e FROM Employee e WHERE e.department.name IN (:dept_engineering, :dept_tool_design, :dept_marketing, :information_servises)", Employee.class)
+                .setParameter("dept_engineering", ENGINEERING_DEPT)
+                .setParameter("dept_tool_design", TOOL_DESIGN_DEPT)
+                .setParameter("dept_marketing", MARKETING_DEPT)
+                .setParameter("information_servises", INFORMATION_SERVICES_DEPT)
+                .getResultList()
+                .forEach(e -> {
+                    e.setSalary(e.getSalary().multiply(BigDecimal.valueOf(1.12)));
+                    System.out.printf("%s %s (%.2f)%n", e.getFirstName(), e.getLastName(), e.getSalary());
+                });
+
+        entityManager.getTransaction().commit();
+        entityManager.close();
+    }
 }
