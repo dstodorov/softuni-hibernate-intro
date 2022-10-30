c4548d9aab9ef28b774fd8cc30f01f7761b2b60c
Dimitar Todorov <d.todorov.vn@gmail.com>
2022-10-30 16:27:46 +0200

    Add exercise

---
+++ src/main/java/FindLatest10Projects.java
@@ -0,0 +1,19 @@
+import entities.Project;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+
+public class FindLatest10Projects {
+    public static void main(String[] args) {
+        EntityManager entityManager = Persistence.createEntityManagerFactory("soft_uni").createEntityManager();
+
+        entityManager.createQuery("SELECT p FROM Project p ORDER BY p.name", Project.class)
+                .setMaxResults(10)
+                .getResultList()
+                .forEach(p -> System.out.printf("Project name: %s%n " +
+                                "\tProject description: %s%n" +
+                                "\tProject Start Date: %s%n" +
+                                "\tProject End Date: %s%n",
+                        p.getName(), p.getDescription(), p.getStartDate().toString(), p.getEndDate() == null ? "null" : p.getEndDate().toString()));
+    }
+}
